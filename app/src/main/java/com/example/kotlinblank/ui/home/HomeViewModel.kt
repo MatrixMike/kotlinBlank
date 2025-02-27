8a05f53f456e6bdc9250d1e8c6a25bfbba02f932
matrixmike <matroxmike@icloud.com>
2025-02-27 21:45:19 +1100

    early commit B

--- app/src/main/java/com/example/kotlinblank/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/kotlinblank/ui/home/HomeViewModel.kt
@@ -3,11 +3,17 @@ package com.example.kotlinblank.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.Locale
+fun String.removeFirstLastChar():
+        String = this.substring(1, this.length -1)
 
 class HomeViewModel : ViewModel() {
-
+    val string1 = "Today is cloudy"
+    val string2 = string1.removeFirstLastChar()
+    var cont = "ALEC"
+//    println("Hi ${cont.toLowerCase()}")
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Hi ${cont.lowercase(Locale.ROOT)}  $string2"
     }
     val text: LiveData<String> = _text
 }
